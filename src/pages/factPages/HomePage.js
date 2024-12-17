6f313de556d7c384c8696a814040b45ea04b0c21
tonnytech <tonnytei4@gmail.com>
2024-12-17 10:09:57 +0300

    Add hands svg image

--- src/pages/factPages/HomePage.js
+++ src/pages/factPages/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 const HomePage = () => {
   return (
     <section>
       <Header />
       <p>Home page</p>
+      <Footer />
     </section>
   );
 };
